b8f7377e15152e406e0f3d148d04302d9209249a
Tommy Saucey <tommysaucey@Air-de-Tommy.lan>
2023-10-01 19:16:11 +0200

    📦 NEW: Add teacher form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-  return <div>Courses pages</div>;
+  return (
+    <div className="p-6">
+      <Link href={"/teacher/create"}>
+        <Button>Nouveau cours</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import * as z from "zod";
+import axios from "redaxios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+const formScema = z.object({
+  title: z.string().min(1, {
+    message: "Le titre est requis.",
+  }),
+});
+
+const TeacherCreatePage = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formScema>>({
+    resolver: zodResolver(formScema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: z.infer<typeof formScema>) => {
+    try {
+      const response = await axios.post("/api/courses", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+    } catch {
+      toast.error("Une erreur est survenue");
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Nom du cours</h1>
+        <p className="text-sm text-slate-600">
+          Comment souhaitez-vous nommer votre cours ? Ne vous inquiétez pas,
+          vous pourrez le changer par la suite.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Titre du cours</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="NextJS 13 - Le guide complet"
+                      {...field}
+                    ></Input>
+                  </FormControl>
+                  <FormDescription>
+                    Le titre de votre cours doit être concis et précis.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button variant={"ghost"} type="button">
+                  Annuler
+                </Button>
+              </Link>
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Continuer
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default TeacherCreatePage;
